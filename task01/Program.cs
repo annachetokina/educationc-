0588cab795a3b73147d435d683993229a529feba
Anna <annachetokina@inbox.ru>
2022-11-15 17:37:54 +0300

    Решение task01 окончательное

--- task01/Program.cs
+++ task01/Program.cs
@@ -7,8 +7,22 @@
 // a = -3 b = 9 -> нет
 
 Console.WriteLine("Введите первое число: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите второе число: ");
 
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (number1 == number2 * number2)
 {
-    if
+    Console.WriteLine("Да!");
 }
+
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
