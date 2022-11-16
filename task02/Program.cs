23feaaeedb4cb558b3453142ae8bcea0245ca990
Anna <annachetokina@inbox.ru>
2022-11-16 16:51:01 +0300

    Решение  task2

--- task02/Program.cs
+++ task02/Program.cs
@@ -8,3 +8,15 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
+if (a == b)
+{
+    Console.WriteLine($"{a} = {b}");
+}
+else if (a > b)
+{
+ Console.WriteLine($" max = {a}");
+}
+else
+{
+ Console.WriteLine($" max = {b}");
+}
