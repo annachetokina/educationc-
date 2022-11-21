aa0b2220f5a67fedc28dfe92068dc7123786dcc2
Anna <annachetokina@inbox.ru>
2022-11-21 15:10:07 +0300

    Решение task10 окончательное

--- task10/Program.cs
+++ task10/Program.cs
@@ -7,22 +7,25 @@ Console.WriteLine("Введите число от 100 до 999:");
 
 int num = Convert.ToInt32(Console.ReadLine());
 
-if( num > 999) 
-Console.WriteLine("Некорректно введенное число");
-
-if(num < 1000)
-Console.WriteLine("Некорректно введенное число");
-
-
 int ShowSecondDigit(int numb)
 
-{
+{   
     int secondDigit = numb % 100;
     int result = secondDigit / 10 ;
     return result;
 }
 
 Console.WriteLine(ShowSecondDigit(num));
+
+// Не могу сообразить, как корректно указать условие, чтобы выводило ошибку, если введено число не в диапазоне [100, 1000].
+// Указанное ниже выдает сообщение пользователю, но функция при этом продолжает считать.
+
+
+// if( num > 999) 
+// Console.WriteLine("Некорректно введенное число");
+
+// if(num < 100)
+// Console.WriteLine("Некорректно введенное число");
 
 
 
