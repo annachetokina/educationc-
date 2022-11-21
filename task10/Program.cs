2870d50f068afa3fa67754b6d18e14588237812e
Anna <annachetokina@inbox.ru>
2022-11-21 14:32:20 +0300

    Решение task10 окончательное

--- task10/Program.cs
+++ task10/Program.cs
@@ -7,11 +7,16 @@ Console.WriteLine("Введите число от 100 до 999:");
 
 int num = Convert.ToInt32(Console.ReadLine());
 
+if( num > 999) 
+Console.WriteLine("Некорректно введенное число");
+
+if(num < 1000)
+Console.WriteLine("Некорректно введенное число");
+
 
 int ShowSecondDigit(int numb)
 
 {
-    
     int secondDigit = numb % 100;
     int result = secondDigit / 10 ;
     return result;
