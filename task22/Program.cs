b82176605d06ba1e80bbc6a1c26725aac4deca6a
Anna <annachetokina@inbox.ru>
2022-11-23 16:07:51 +0300

    Решение task22 дополненное

--- task22/Program.cs
+++ task22/Program.cs
@@ -4,15 +4,39 @@
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1,4
 
-Console.WriteLine("Введите число:");
-int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
 
-void Square(int n)
+// void Square(int n)
 
+// {
+//     for (int i = 1; i <= n ; i++)
+// {
+//     Console.WriteLine($"{i} -> {i*i}");
+// }
+// }
+// Square(num1);
+
+int n = -1;
+while (n < 1)
 {
-    for (int i = 1; i <= n ; i++)
+    Console.WriteLine("Введите число");
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n < 1) Console.WriteLine("Введены неверные данные");
+}
+
+string SqrTable(int n1)
 {
-    Console.WriteLine($"{i} -> {i*i}");
+    int count = 1;
+    string table = "";
+    while (count <= n1)
+    {
+        table = table + $"{count}|{count * count} \n";
+        count++;
+    }
+    return table;
 }
-}
-Square(num1);
+
+string sqrTable = SqrTable(n);
+Console.WriteLine(sqrTable);
+
