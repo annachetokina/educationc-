81f8b1b6d3f14d56cd0857b17264be9285855896
Anna <annachetokina@inbox.ru>
2022-11-28 17:24:23 +0300

    task27

--- task27/Program.cs
+++ task27/Program.cs
@@ -13,13 +13,15 @@ int SumDigit(int num)
 {
   int count = 0;
   int sum = 0;
+  int a1 = num / 10;
   while (num > 0)
    {
     num = num % 10;
     sum = sum + num;
     count++;
    }
-  return sum;
+  int res = a1 + sum;
+  return res;
 }
 
 if (number < 0) number = -number;
